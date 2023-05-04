be8a47e62a13cd2b70d9da092881f6d2effe4133
lakindu62 <lakindu62@gmail.com>
2023-05-04 12:45:37 +0530

    completed v1

--- src/movie.js
+++ src/movie.js
@@ -1,37 +1,57 @@
-
+// } "<img src='/public/add.svg' /> Add to Watchlist"     "<img src='/public/remove.svg' /> Remove" }
 
 class Movie{
 
-    constructor(data){
-        Object.assign(data , this)
+    constructor(data , isFromLocalStorage = false){
+        Object.assign(this , data)
+        this.isFromLocalStorage = isFromLocalStorage
+        this.added = false
+        this.inLocalStorage = false
     }
 
     getHtml(){
-        const {Poster,Title,imdbRating,Runtime,Genre,imdbID,Plot} = this;
+        const {Poster,Title,imdbRating,Runtime,Genre,imdbID,Plot , isFromLocalStorage , added , inLocalStorage} = this;
         return`
-                <div class=" flex items-start  rounded-xl">
-                        <img class="h-[300px]" src="${Poster}" />
-                        <div class='movieInfo--container'>
-                            <div>
-                                <h1>${Title}</h1>
-                                <span><img src="" /> <p>${imdbRating}</p></span>
+                <div class=" movie flex items-start  rounded-xl mb-5 mt-10">
+                        <img class="h-[300px] w-[202px] object-cover m-auto relative left-0 right-0 " src="${Poster}" />
+                        <div class='movieInfo--container ml-10 flex flex-col justify-center h-[100%] '>
+                            <div class="flex  justify-between mb-5">
+                                <h1 class="text-2xl  font-bold">${Title}</h1>
+                                <span class="flex gap-2"><img src="/public/star.svg" /> <p>${imdbRating}</p></span>
                             </div>
-                            <div>
-                                <span>${Runtime}</span>
-                                <span>${Genre}</span>
+                            <div class="flex justify-between mb-5">
                                 <div>
-                                    <img src="" />
-                                    <button class="btn--watchlist" id="${imdbID}">Watchlist</button>
+                                    <span class=" text-base">${Runtime}</span>
+                                    <span class="ml-5 text-base">${Genre}</span>
+                                </div>
+                                <div class="ml-10 flex justify-between">
+                                    
+                                    <button  
+                                    
+                                    ${isFromLocalStorage ? "data-inLocal ='TRUE' " :"data-inLocal ='false'"}
+                                    class="btn--watchlist flex items-center gap-2" 
+                                    id="${imdbID}"
+                                    
+                                        >
+                                        
+                                        ${inLocalStorage || isFromLocalStorage ? "<img src='/public/remove.svg' /> Remove" 
+                                        : "<img src='/public/add.svg' /> Watchlist" }
+                                        
+                                    
+                                    </button>
                                 </div>
                             
                             </div>
-                            <span>${Plot}</span>
+                            <span class="text-[#A5A5A5]">${Plot}</span>
                         </div>
 
 
                     </div>
+                    <hr />
         `
     }
+
+
 
 
 }
